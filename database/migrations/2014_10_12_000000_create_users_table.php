b4e1f75c9f7bbddfbdb5b83a2c2a81feaed058c1
Masiur Rahman Siddiki <masiur.sust@csesociety.org>
2016-04-22 23:47:37 +0600

    schema updated

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('fb_id')->nullable();         
             $table->string('email')->unique();
             $table->string('password', 60);
+            $table->integer('active')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
